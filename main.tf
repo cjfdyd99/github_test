2a15d6ca83b70b2b725394de32dce0838b2cdec8
Frahn_Park <cjfdyd99@gmail.com>
2023-01-11 14:56:59 +0900

    [Update in 2023/01/11]

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "ap-northeast-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0bbc954141b39fb71"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-02db9680568fe55ec"
+
+  tags = {
+    Name = "Terraform_Test_EC2"
+  }
+}
